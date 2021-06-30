1ff3fb481f95f67862de85d7c845295d9e3f17a3
Mona Lisa <isaac11195@gmail.com>
2021-06-30 11:46:33 -0400

    add new commands

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect('mongodb+srv://budget_2021:Vegit@39@cluster0.o5kl7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useFindAndModify: false
 });
